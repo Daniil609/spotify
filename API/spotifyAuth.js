1808af681dac2de36ee5d0cd8193775529bda4c7
Tomashchik Daniil <tomashchik032@gmail.com>
2022-05-28 10:51:37 +0300

    add route

--- API/spotifyAuth.js
+++ API/spotifyAuth.js
@@ -1,8 +1,7 @@
 const client_id = "930bd795ba644df0b70e0e0f838bbdca";
-const redirect_uri = "https%3A%2F%2Fdaniil609.github.io%2Fspotify%2Fhtml%2Fhome.html";
-const unencoded_redirect_uri = "https://daniil609.github.io/spotify/html/home.html";
+const redirect_uri = "https%3A%2F%2Fdaniil609.github.io%2Fspotify";
+const unencoded_redirect_uri = "https://daniil609.github.io/spotify";
 const scopes = "playlist-read-private";
-
 
 export const login = async () => {
   window.open(
@@ -19,10 +18,9 @@ export const spotifyAuthorization = async (code) => {
       grant_type: "authorization_code",
     }),
     headers: {
-      Authorization: "Basic " + btoa(client_id + ":" + "b28cf19380e94552a0d30d88b434fd92"),
+      Authorization:
+        "Basic " + btoa(client_id + ":" + "b28cf19380e94552a0d30d88b434fd92"),
       "Content-Type": "application/x-www-form-urlencoded",
     },
   }).then((response) => response.json());
-
 };
-
